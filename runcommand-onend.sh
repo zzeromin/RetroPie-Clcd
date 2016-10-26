01e173e365a92f010b5f728cf0af2ee74bafe2f2
zzeromin <zzeromin@gmail.com>
2016-10-26 10:28:02 +0900

    Create runcommand-onend.sh

---
+++ runcommand-onend.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# edited by zzeromin ( 2016-10-14 )
+# special thanks to zerocool
+# Reference:
+# runcommand of Retorpie:  https://github.com/retropie/retropie-setup/wiki/runcommand
+# edit and path: $ sudo nano /opt/retropie/configs/all/runcommand-onend.sh
+
+sed '1,2d' /dev/shm/runcommand.log > /dev/shm/runcommand.log
+
+echo "notice" >&2
+echo "PLZ GO TO SLEEP!" >&2
